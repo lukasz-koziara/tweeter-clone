895d77bdf4c80e43cdb21dfc9cf6be2ffe4d6d74
lukasz-koziara <lukasz.koziara88@gmail.com>
2018-10-09 22:25:55 +0200

    added micropost entity

---
+++ src/Entity/MicroPost.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MicroPostRepository")
+ * @ORM\Table()
+ */
+class MicroPost
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string" ,length=280)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+}
